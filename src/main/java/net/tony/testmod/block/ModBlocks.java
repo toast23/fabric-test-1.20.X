dcb7c711a8958312ddceef239e2fd355f23b5f81
Tony <tony.yao23@ycdsbk12.ca>
2025-05-10 18:57:02 -0400

    added falling to the dog block

--- src/main/java/net/tony/testmod/block/ModBlocks.java
+++ src/main/java/net/tony/testmod/block/ModBlocks.java
@@ -4,6 +4,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.FallingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
@@ -14,7 +15,7 @@ import net.tony.testmod.TestMod;
 
 public class ModBlocks {
     public static final Block DOG_BLOCK = registerBlock("dog_block",
-            new Block(FabricBlockSettings.copyOf(Blocks.ANVIL)));
+            new FallingBlock(FabricBlockSettings.copyOf(Blocks.ANVIL)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
---
+++ src/main/java/net/tony/testmod/block/custom/SoundBlock.java
@@ -0,0 +1,12 @@
+package net.tony.testmod.block.custom;
+
+import net.minecraft.block.Block;
+
+
+
+public class SoundBlock extends Block {
+    public SoundBlock(Settings settings) {
+        super(settings);
+    }
+
+}
